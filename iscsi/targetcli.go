595df5c42959bbf4f19f117a77a2ec3e9679d351
rorschach <njutree@gmail.com>
2022-11-16 16:49:25 +0800

    add publish volume and mount lun

--- iscsi/targetcli.go
+++ iscsi/targetcli.go
@@ -8,11 +8,16 @@ import (
 
 var (
 	openIscsiDir = "cd /iscsi"
-	openAclsDir  = "cd tpg1/acls"
+	openBlockDir = "cd /backstores/block"
+
+	// must under target dir
+	openAclsDir = "cd tpg1/acls"
+	openLunsDir = "cd tpg1/luns"
 
 	lsCmd = "ls"
 
 	createCmd      = "create %s"
+	createBlockCmd = "create %s %s"
 	cdCmd          = "cd %s"
 	setUserIDCmd   = "set auth userid=%s"
 	setPasswordCmd = "set auth password=%s"
@@ -53,24 +58,36 @@ func ListTarget() ([]string, error) {
 	return targetList, nil
 }
 
-func SetUpTargetAcl(targetName, username, password string) (string, error) {
+func SetUpTargetAcl(target, username, password string) (string, error) {
 	cmd := NewExecCmd()
 	cmd.Add(openIscsiDir)
-	cmd.AddFormat(cdCmd, targetName)
+	cmd.AddFormat(cdCmd, target)
 	cmd.Add(openAclsDir)
 	// create acls
-	cmd.AddFormat(createCmd, targetName)
-	cmd.AddFormat(cdCmd, targetName)
+	cmd.AddFormat(createCmd, target)
+	cmd.AddFormat(cdCmd, target)
 	// set username and password
 	cmd.AddFormat(setUserIDCmd, username)
 	cmd.AddFormat(setPasswordCmd, password)
 	return cmd.Exec()
 }
 
-func PublicBlockDevice(name, device string) {
+// PublicBlockDevice publish device as block device
+func PublicBlockDevice(disk, device string) (string, error) {
+	cmd := NewExecCmd()
+	cmd.Add(openBlockDir)
+	cmd.AddFormat(createBlockCmd, disk, device)
+	return cmd.Exec()
 
 }
 
-func MountLun(name string) string {
-	return ""
+// MountLun mount device as lun Only support block device
+func MountLun(target, disk string) (string, error) {
+	disk = "/backstores/block/" + disk
+	cmd := NewExecCmd()
+	cmd.Add(openIscsiDir)
+	cmd.AddFormat(cdCmd, target)
+	cmd.Add(openLunsDir)
+	cmd.AddFormat(createCmd, disk)
+	return cmd.Exec()
 }
