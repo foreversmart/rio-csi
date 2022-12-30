f19445a4a5e91ee507aa0b2ccb1a7c3a33da1468
rorschach <njutree@gmail.com>
2022-12-30 17:47:17 +0800

    add list acls

--- iscsi/targetcli_auth.go
+++ iscsi/targetcli_auth.go
@@ -1,5 +1,7 @@
 package iscsi
 
+import "strings"
+
 func SetDiscoveryAuth(username, password string) error {
 	cmd := NewExecCmd()
 	cmd.Add(openIscsiDir)
@@ -21,4 +23,35 @@ func SetUpTargetAcl(target, client, username, password string) (string, error) {
 	cmd.AddFormat(setUserIDCmd, username)
 	cmd.AddFormat(setPasswordCmd, password)
 	return cmd.Exec()
+}
+
+// ListTargetAcl get target acl rules
+func ListTargetAcl(target string) (aclInitiator []string, err error) {
+	cmd := NewExecCmd()
+	cmd.Add(openIscsiDir)
+	cmd.AddFormat(cdCmd, target)
+	cmd.Add(openAclsDir)
+	// ls
+	cmd.Add(lsCmd)
+	out, err := cmd.Exec()
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(out, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, "o- iqn.") {
+			splitLoc := strings.Index(line, " .....")
+			if splitLoc == -1 {
+				continue
+			}
+
+			t := line[:splitLoc]
+			t = strings.TrimPrefix(t, "o- ")
+			aclInitiator = append(aclInitiator, t)
+		}
+	}
+
+	return
 }
