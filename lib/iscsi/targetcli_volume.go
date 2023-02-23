2f87b158a48fc5a5f483f6b18e0c82ba29dada50
foreversmart <njutree@gmail.com>
2023-02-23 16:36:08 +0800

    remove public block device target input

--- lib/iscsi/targetcli_volume.go
+++ lib/iscsi/targetcli_volume.go
@@ -1,20 +1,16 @@
 package iscsi
 
 // PublicBlockDevice publish device as block device
-func PublicBlockDevice(target, disk, device string) (string, error) {
+func PublicBlockDevice(disk, device string) (string, error) {
 	cmd := NewExecCmd()
-	cmd.Add(openIscsiDir)
-	cmd.AddFormat(cdCmd, target)
 	cmd.Add(openBlockDir)
 	cmd.AddFormat(createBlockCmd, disk, device)
 	return cmd.Exec()
 }
 
 // UnPublicBlockDevice publish device as block device
-func UnPublicBlockDevice(target, disk string) (string, error) {
+func UnPublicBlockDevice(disk string) (string, error) {
 	cmd := NewExecCmd()
-	cmd.Add(openIscsiDir)
-	cmd.AddFormat(cdCmd, target)
 	cmd.Add(openBlockDir)
 	cmd.AddFormat(deleteCmd, disk)
 	return cmd.Exec()
