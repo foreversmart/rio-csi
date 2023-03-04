8860d8b3b52f180e4af1b2604f884d6f20bbe0bc
foreversmart <njutree@gmail.com>
2023-03-04 22:27:10 +0800

    add node view calc socre

--- driver/scheduler/node_view.go
+++ driver/scheduler/node_view.go
@@ -3,6 +3,7 @@ package scheduler
 import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	apis "qiniu.io/rio-csi/api/rio/v1"
+	"qiniu.io/rio-csi/driver"
 	"regexp"
 )
 
@@ -13,6 +14,21 @@ type NodeView struct {
 	TotalSize   resource.Quantity `json:"total_size"`
 	TotalFree   resource.Quantity `json:"total_free"`
 	MaxFree     resource.Quantity `json:"max_free"`
+	Score       int64             `json:"score"`
+}
+
+// CalcScore calc node score used storage weight is -1 free storage weight 1
+// volume Num weight is -1 * 100 Gi, snapshot Num is -1 * 100 Gi
+// the more lv num the score is lower
+// the more free disk storage the score is higher
+// the more usage disk storage the score is lower
+func (n *NodeView) CalcScore() {
+	used := n.TotalSize
+	used.Sub(n.TotalFree)
+	free := n.TotalFree
+	free.Sub(used)
+	score := free.Value() - 100*driver.Gi*(n.VolumeNum+n.SnapshotNum)
+	n.Score = score
 }
 
 var (
--- driver/scheduler/scheduler.go
+++ driver/scheduler/scheduler.go
@@ -1,14 +1,13 @@
 package scheduler
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/client-go/dynamic/dynamicinformer"
-	"k8s.io/client-go/dynamic/dynamiclister"
+	"github.com/container-storage-interface/spec/lib/go/csi"
 	apis "qiniu.io/rio-csi/api/rio/v1"
+	"qiniu.io/rio-csi/client"
 	"qiniu.io/rio-csi/driver/dparams"
+	"qiniu.io/rio-csi/logger"
+	"sort"
 	"sync"
-	"time"
 )
 
 var (
@@ -19,17 +18,50 @@ type VolumeScheduler struct {
 }
 
 func NewVolumeScheduler() {
-	nodeInformerFactory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(client.DefaultClient.DynamicClient, 5*time.Minute,
-		namespace, func(options *metav1.ListOptions) {
-			options.FieldSelector = fields.OneTermEqualSelector("metadata.name", nodeID).String()
-		})
 
-	nodeInformer := nodeInformerFactory.ForResource(nodeResource).Informer()
-	lister := dynamiclister.New(nodeInformer.GetIndexer(), nodeResource)
 }
 
 // GetNode BalancedResourceAllocation
-func GetNode(param *dparams.VolumeParams) (node *apis.RioNode, err error) {
+func (s *VolumeScheduler) GetNode(req *csi.CreateVolumeRequest, param *dparams.VolumeParams) (node *apis.RioNode, err error) {
+	nodes, err := client.DefaultInformer.Rio().V1().RioNodes().Lister().List(nil)
+	if err != nil {
+		logger.StdLog.Errorf("list node error", err)
+		return nil, err
+	}
 
 	return
+}
+
+func (s *VolumeScheduler) Runner() {
+	nodes, err := client.DefaultInformer.Rio().V1().RioNodes().Informer().AddEventHandler()
+}
+
+func (s *VolumeScheduler) Schedule(req *csi.CreateVolumeRequest, param *dparams.VolumeParams) {
+	nodes, err := client.DefaultInformer.Rio().V1().RioNodes().Lister().List(nil)
+	if err != nil {
+		logger.StdLog.Errorf("list node error", err)
+		return nil, err
+	}
+
+	filterNodes, err := filterTopologyRequirement(req.AccessibilityRequirements)
+	if err != nil {
+		logger.StdLog.Errorf("filterTopologyRequirement %v", err)
+		return
+	}
+
+}
+
+func (s *VolumeScheduler) NodeSort(req *csi.CreateVolumeRequest) (nodes []*NodeView) {
+	nodes = make([]*NodeView, len(NodeViewMap))
+	for _, node := range NodeViewMap {
+		node.CalcScore()
+		nodes = append(nodes, node)
+	}
+
+	//
+
+	// sort the filtered node map
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].Score() < fmap[j].Value
+	})
 }
