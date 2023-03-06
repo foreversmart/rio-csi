d2d573f36dccf948e7863c335bd58c9462d30282
foreversmart <njutree@gmail.com>
2023-03-06 23:16:58 +0800

    add volume update handle

--- driver/scheduler/volume_handler.go
+++ driver/scheduler/volume_handler.go
@@ -4,14 +4,31 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	apis "qiniu.io/rio-csi/api/rio/v1"
+	"qiniu.io/rio-csi/crd"
 	"qiniu.io/rio-csi/logger"
 )
+
+var CreatedVolumeList []string
 
 func addVolume(obj interface{}) {
 
 }
 
 func updateVolume(oldObj, newObj interface{}) {
+	newVolume, ok := VolumeStructuredObject(newObj)
+	if !ok {
+		logger.StdLog.Errorf("cant get new volume info %v", newObj)
+		return
+	}
+
+	switch newVolume.Status.State {
+	case crd.StatusReady, crd.StatusCreated, crd.StatusCloning:
+		if _, ok := CacheVolumeMap[newVolume.Name]; ok {
+			Lock.Lock()
+			defer Lock.Unlock()
+			CreatedVolumeList = append(CreatedVolumeList, newVolume.Name)
+		}
+	}
 
 }
 
