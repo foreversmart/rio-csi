dccd6b31201dcb0a48bf73088a1092d43426986d
foreversmart <njutree@gmail.com>
2023-04-18 17:57:18 +0800

    add driver config

--- driver/config/config.go
+++ driver/config/config.go
@@ -1,88 +1,48 @@
-/*
-Copyright © 2019 The OpenEBS Authors
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package config
 
-// Config struct fills the parameters of request or user input
+// Config struct define how driver running
 type Config struct {
-	// DriverName to be registered at CSI
-	DriverName string
-
-	// PluginType flags if the driver is
-	// it is a node plugin or controller
-	// plugin
-	PluginType string
-
-	// Version of the CSI controller/node driver
-	Version string
-
-	// Endpoint on which requests are made by kubelet
-	// or external provisioner
-	//
-	// NOTE:
-	//  - Controller/node plugin will listen on this
-	//  - This will be a unix based socket
-	Endpoint string
-
-	// NodeID helps in differentiating the nodes on
-	// which node drivers are running. This is useful
-	// in case of topologies and publishing or
-	// unpublishing volumes on nodes
-	NodeID string
-
+	// Namespace string
+	Namespace string `yaml:"namespace"`
 	// SetIOLimits if set to true, directs the driver
 	// to set iops, bps limits on a pod using a volume
 	// provisioned on its node. For this to work,
 	// CSIDriver.Spec.podInfoOnMount must be set to 'true'
-	SetIOLimits bool
-
+	SetIOLimits bool `yaml:"set_io_limits"`
 	// ContainerRuntime informs the driver of the container runtime
 	// used on the node, so that the driver can make assumptions
 	// about the cgroup path for a pod requesting a volume mount
-	ContainerRuntime string
+	ContainerRuntime string `yaml:"container_runtime"`
 
-	// RIopsLimitPerGB provides read iops rate limits per volume group type
+	// ReadIopsLimit provides read iops rate limits per GB in specific volume group type
 	// as a string slice, in the form ["vg1-prefix=100", "vg2-prefix=200"]
-	RIopsLimitPerGB *[]string
+	ReadIopsLimit *[]string `yaml:"read_iops_limit"`
 
-	// WIopsLimitPerGB provides write iops rate limits per volume group type
+	// WriteIopsLimit provides write iops rate limits per GB in specific volume group type
 	// as a string slice, in the form ["vg1-prefix=100", "vg2-prefix=200"]
-	WIopsLimitPerGB *[]string
+	WriteIopsLimit *[]string
 
-	// RBpsLimitPerGB provides read bps rate limits per volume group type
+	// ReadBpsLimit provides read bps rate limits per GB in specific volume group type
 	// as a string slice, in the form ["vg1-prefix=100", "vg2-prefix=200"]
-	RBpsLimitPerGB *[]string
+	ReadBpsLimit *[]string
 
-	// WBpsLimitPerGB provides read bps rate limits per volume group type
+	// WriteBpsLimit provides read bps rate limits per GB in specific volume group type
 	// as a string slice, in the form ["vg1-prefix=100", "vg2-prefix=200"]
-	WBpsLimitPerGB *[]string
-
-	// The TCP network address where the prometheus metrics endpoint will listen (example: `:9080`).
-	// The default is empty string, which means metrics endpoint is disabled.
-	ListenAddress string
+	WriteBpsLimit *[]string
 
 	// The HTTP path where prometheus metrics will be exposed. Default is `/metrics`.
 	MetricsPath string
 
+	// MetricsAddr specific metric server listen addr
+	MetricsAddr string
+	// MetricsAddr specific probe server listen addr
+	ProbeAddr string
+
 	// Exclude metrics about the exporter itself (process_*, go_*).
 	DisableExporterMetrics bool
-}
 
-// Default returns a new instance of config
-// required to initialize a driver instance
-func Default() *Config {
-	return &Config{}
+	// IscsiUsername specific iscsi username for iscsi auth
+	IscsiUsername string
+	// IscsiPasswd specific iscsi password for iscsi auth
+	IscsiPasswd string
 }
