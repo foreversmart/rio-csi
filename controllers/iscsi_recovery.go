d1864b1472c550ec99a03cc7450a20b38566d93e
foreversmart <njutree@gmail.com>
2023-05-22 17:27:47 +0800

    add iscsi init and recovery

---
+++ controllers/iscsi_recovery.go
@@ -0,0 +1,82 @@
+package controllers
+
+import (
+	"fmt"
+	apis "qiniu.io/rio-csi/api/rio/v1"
+	"qiniu.io/rio-csi/crd"
+	"qiniu.io/rio-csi/lib/iscsi"
+	"qiniu.io/rio-csi/logger"
+)
+
+// CheckAndRecoveryDisk check all disk from csi cr disk and recovery disk status
+func CheckAndRecoveryDisk(iscsiUsername, iscsiPassword string) {
+	targets, err := iscsi.ListTarget()
+	if err != nil {
+		logger.StdLog.Error("List target error", err)
+		return
+	}
+
+	targetsMap := make(map[string]bool)
+	for _, v := range targets {
+		targetsMap[v] = true
+	}
+
+	logger.StdLog.Info("Check Disk IScsi start")
+	skip := ""
+	limit := int64(100)
+	for {
+		resp, conStr, err := crd.ListVolumes(skip, limit)
+		if err != nil {
+			logger.StdLog.Errorf("ListVolumes skip %d limit %d error %v", skip, limit, err)
+			return
+		}
+
+		for _, vol := range resp {
+			CheckAndRecoveryDiskIscsi(vol, iscsiUsername, iscsiPassword, targetsMap)
+		}
+
+		if conStr == "" {
+			break
+		}
+
+		skip = conStr
+	}
+	logger.StdLog.Info("Check Disk IScsi Finish")
+}
+
+// CheckAndRecoveryDiskIscsi check disk status and do iscsi recovery
+func CheckAndRecoveryDiskIscsi(vol apis.Volume, iscsiUsername, iscsiPassword string, targetMap map[string]bool) {
+	target := vol.Spec.IscsiTarget
+
+	// check target already exist or abnormal return
+	if vol.Spec.IscsiTarget == "" || targetMap[target] {
+		return
+	}
+
+	_, err := iscsi.CreateTarget(target)
+	if err != nil {
+		logger.StdLog.Errorf("CheckAndRecoveryDisk: CreateTarget %s error %v", target, err)
+	}
+
+	// check ACL
+	err = CreateTargetAcl(vol.Namespace, vol.Spec.IscsiTarget, iscsiUsername, iscsiPassword)
+	if err != nil {
+		logger.StdLog.Error(err, fmt.Sprintf("CheckAndRecoveryDisk: CreateTargetAcl %v", err))
+	}
+
+	// public block device
+	device := getVolumeDevice(&vol)
+	_, err = iscsi.PublicBlockDevice(vol.Name, device)
+	if err != nil {
+		logger.StdLog.Error(err, fmt.Sprintf("PublicBlockDevice target %s, vol %s, device %s error: %v",
+			vol.Spec.IscsiTarget, vol.Name, device, err))
+	}
+
+	// mount lun device
+	_, err = iscsi.MountLun(vol.Spec.IscsiTarget, vol.Name)
+	if err != nil {
+		logger.StdLog.Error(err, fmt.Sprintf("MountLun target %s, vol %s,  error: %v",
+			vol.Spec.IscsiTarget, vol.Name, err))
+	}
+
+}
--- manager/manager.go
+++ manager/manager.go
@@ -1,21 +1,5 @@
 package manager
 
-/*
-Copyright 2022.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 import (
 	"os"
 	"qiniu.io/rio-csi/logger"
@@ -56,6 +40,9 @@ func StartManager(nodeID, namespace, metricsAddr, probeAddr, iscsiUsername, iscs
 		logger.StdLog.Errorf("cant new node manager %s %s error %v", nodeID, namespace, err)
 		os.Exit(1)
 	}
+
+	// start check disk status and recovery
+	controllers.CheckAndRecoveryDisk(iscsiUsername, iscsiPassword)
 
 	// start node manager
 	go nodeManager.Start()
